4ee5433adf0839897de50c70cc79c4ffb26dd397
Niro Karunakaran <niro@tailit.com>
2021-07-22 10:53:52 +0200

    added styling

--- website/views.py
+++ website/views.py
@@ -15,7 +15,7 @@ songData = []
 
 def getAllSongs(request):
     getSongs("37i9dQZF1DX1i3hvzHpcQV", "ta")
-    #getSongs("37i9dQZEVXbJvfa0Yxg7E7")
+    getSongs("37i9dQZEVXbJvfa0Yxg7E7", "no")
     getSongs("37i9dQZEVXbNG2KDcFcKOF", "en")
     getSongs("41v7j1rKJmf2n0wBCWt43j", "kr")
 
@@ -36,41 +36,3 @@ def getSongs(id, lang):
         songData.append(songInfo)
 
 
-'''def korSongs(request):
-    results = sp.playlist_items(playlist_id='41v7j1rKJmf2n0wBCWt43j', fields=None, limit=5, offset=0, market=None,
-                                additional_types=('track',))
-    for item in enumerate(results['items']):
-        songInfo = []
-        songInfo.append(item[1]['track']['album']['external_urls']['spotify'])
-        songInfo.append(item[1]['track']['album']['images'][1]['url'])
-        songInfo.append(item[1]['track']['album']['name'])
-        korData.append(songInfo)
-    return render(request, "website/index.html",
-                  {"kor": korData})
-
-
-def norSongs(request):
-    results = sp.playlist_items(playlist_id='37i9dQZEVXbJvfa0Yxg7E7', fields=None, limit=5, offset=0, market=None,
-                                additional_types=('track',))
-    for item in enumerate(results['items']):
-        songInfo = []
-        songInfo.append(item[1]['track']['album']['external_urls']['spotify'])
-        songInfo.append(item[1]['track']['album']['images'][1]['url'])
-        songInfo.append(item[1]['track']['album']['name'])
-        norData.append(songInfo)
-    return render(request, "website/index.html",
-                  {"norw": norData})
-
-
-def enSongs(request):
-    results = sp.playlist_items(playlist_id='37i9dQZEVXbNG2KDcFcKOF', fields=None, limit=5, offset=0, market=None,
-                                additional_types=('track',))
-    for item in enumerate(results['items']):
-        songInfo = []
-        songInfo.append(item[1]['track']['album']['external_urls']['spotify'])
-        songInfo.append(item[1]['track']['album']['images'][1]['url'])
-        songInfo.append(item[1]['track']['album']['name'])
-        enData.append(songInfo)
-    return render(request, "website/index.html",
-                  {"eng": enData})
-'''
