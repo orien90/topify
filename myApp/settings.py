8b12b55a38eaa072c205ccf25e097a69cdfa24bd
Niro Karunakaran <niro@tailit.com>
2021-07-22 12:26:15 +0200

    added dependency fix

--- myApp/settings.py
+++ myApp/settings.py
@@ -119,3 +119,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DISABLE_COLLECTSTATIC=1
