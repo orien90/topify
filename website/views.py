00e01034492d315ff11e75c9c83e1500702c84d7
Niro Karunakaran <niro@tailit.com>
2021-07-25 08:54:44 +0200

    added comments and changed variable names with correct naming conventions

--- website/views.py
+++ website/views.py
@@ -6,37 +6,33 @@ import re
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="6ce04d774cb14eb5a05990989db5dab1",
                                                            client_secret="921f09aa289240fbab1764f4a55ba7aa"))
 
+#idList = {'ta': '37i9dQZF1DX1i3hvzHpcQV', 'no': '37i9dQZEVXbJvfa0Yxg7E7', 'en': '37i9dQZEVXbNG2KDcFcKOF',
+          #'kr': '41v7j1rKJmf2n0wBCWt43j'}
 
-idList = {'ta': '37i9dQZF1DX1i3hvzHpcQV', 'no': '37i9dQZEVXbJvfa0Yxg7E7', 'en': '37i9dQZEVXbNG2KDcFcKOF',
-          'kr': '41v7j1rKJmf2n0wBCWt43j'}
-
-# current = sp.currently_playing()
-songData = []
+song_data = []
 
 
-def getAllSongs(request):
-    getSongs("37i9dQZF1DX1i3hvzHpcQV", "ta")
-    getSongs("37i9dQZEVXbJvfa0Yxg7E7", "no")
-    getSongs("37i9dQZEVXbNG2KDcFcKOF", "en")
-    getSongs("41v7j1rKJmf2n0wBCWt43j", "kr")
-
+# get four different playlist from Spotify
+def get_all_songs(request):
+    get_songs("37i9dQZF1DX1i3hvzHpcQV", "ta")
+    get_songs("37i9dQZEVXbJvfa0Yxg7E7", "no")
+    get_songs("37i9dQZEVXbNG2KDcFcKOF", "en")
+    get_songs("41v7j1rKJmf2n0wBCWt43j", "kr")
     return render(request, "website/index.html",
-                  {"results": songData})
+                  {"results": get_songs})
 
 
-def getSongs(id, lang):
-    results = []
+def get_songs(id, lang):
     results = sp.playlist_items(playlist_id=id, fields=None, limit=5, offset=0, market=None,
                                 additional_types=('track',))
+    # appends each albums title, image and url to song_info
     for item in enumerate(results['items']):
         title = re.split(r'\([^)]*\)', item[1]['track']['album']['name'])
         title = title[0]
-        songInfo = []
-        songInfo.append(item[1]['track']['album']['external_urls']['spotify'])
-        songInfo.append(item[1]['track']['album']['images'][1]['url'])
-        songInfo.append(title)
-        songInfo.append(lang)
-        if not songInfo in songData:
-            songData.append(songInfo)
-
-
+        song_info = []
+        song_info.append(item[1]['track']['album']['external_urls']['spotify'])
+        song_info.append(item[1]['track']['album']['images'][1]['url'])
+        song_info.append(title)
+        song_info.append(lang)
+        if not song_info in song_data:
+            song_data.append(song_info)
